c6c7d65415cfc74c941e214492bc851fa0c4b677
Anastasya <anastasiaostin@mail.ru>
2023-01-22 19:30:43 +0300

    Исправление

--- Ex13/Program.cs
+++ Ex13/Program.cs
@@ -5,9 +5,11 @@
 Console.Write("Введите любое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(num);
-if (anyNumberText.Length > 2){
+if (anyNumberText.Length > 2)
+{
   Console.WriteLine("Третья цифра: " + anyNumberText[2]);
 }
-else {
+else 
+{
   Console.WriteLine("Третья цифра отсутствует");
 }
